50105ba0f916a7725fa39726a033218f0f4ba58e
Alexandre de Limas Santana <alexandre.limas.santana@gmail.com>
2019-05-27 18:28:24 -0300

    ready for charm tests

--- include/mogslib/mogslib.h
+++ include/mogslib/mogslib.h
@@ -3,21 +3,21 @@
 #include <tuple>
 #include <string>
 
-#include <abstractions/traits/libgomp_default.h>
+#include <abstractions/traits/charm_default.h>
 
-namespace MOGSLib {  using Traits = LibgompDefault;  }
+namespace MOGSLib {  using Traits = CharmDefault;  }
 
-#include <abstractions/contexts/libgomp_multiloop.h>
+#include <abstractions/contexts/charm.h>
 
-#include <model/schedulers/buffered_binlpt.h>
+#include <model/schedulers/greedy.h>
 
 #include "macros.h"
 
 namespace MOGSLib {
 
 struct API {
-  using Contexts = std::tuple<Ctx(LibGOMPMultiloop)>;
-  using Schedulers = std::tuple<Sched(BufferedBinLPT, 0)>;
+  using Contexts = std::tuple<Ctx(Charm)>;
+  using Schedulers = std::tuple<Sched(Greedy, 0)>;
 
   static Contexts contexts;
   static Schedulers schedulers;
@@ -32,7 +32,7 @@ struct API {
   }
 
   inline static void work(const std::string &name) {
-    if(test_scheduler("buffered_binlpt", name))
+    if(test_scheduler("greedy", name))
 			do_work<0>();
     else {
       throw std::string("[MOGSLib] Invalid scheduler name.");
